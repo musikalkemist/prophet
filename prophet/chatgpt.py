6a4acad6e173acb112a21cc44dd375411b6db0ad
Valerio Velardo <velardovalerio@gmail.com>
2022-12-14 16:03:06 +0100

    chore: reformat files

--- prophet/chatgpt.py
+++ prophet/chatgpt.py
@@ -4,7 +4,7 @@ from revChatGPT.revChatGPT import Chatbot
 
 
 class ChatGPT:
-    """This class is a wrapper for the Chatbot class from revChatGPT. """
+    """This class is a wrapper for the Chatbot class from revChatGPT."""
 
     def __init__(self):
         self.chatbot = Chatbot({})
@@ -12,7 +12,7 @@ class ChatGPT:
     def get_response(
         self, message_prompt: str, conversation_id: str = None, parent_id: str = None
     ) -> str:
-        response =  self.chatbot.get_chat_response(
+        response = self.chatbot.get_chat_response(
             message_prompt,
             output="text",
             conversation_id=conversation_id,
